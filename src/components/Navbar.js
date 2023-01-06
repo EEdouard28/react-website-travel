866781d3fa07b51973c8c3ba9934f3a6133a3555
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2023-01-06 15:42:16 -0500

    btn function created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <nav className="navbar">
@@ -9,6 +13,45 @@ function Navbar() {
           <Link to="/" className="navbar-logo">
             TRVL
           </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {/* hamburger menu */}
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          {/* navmenu disappear */}
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
