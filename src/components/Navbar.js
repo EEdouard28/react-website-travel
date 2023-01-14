edbd991053f1b8f56719f8d6a1590c878d676f02
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2023-01-14 12:53:04 -0500

    navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            TRVL
+            TRVL THE WORLD
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             {/* hamburger menu */}
